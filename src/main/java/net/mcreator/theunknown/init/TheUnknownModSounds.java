0ffedc12b07908fd5bd83fe2648a799bd14fcecf
brambleclaw1 <gavinhoeksema@gmail.com>
2023-03-29 10:03:27 -0600

    added music

--- src/main/java/net/mcreator/theunknown/init/TheUnknownModSounds.java
+++ src/main/java/net/mcreator/theunknown/init/TheUnknownModSounds.java
@@ -16,4 +16,7 @@ import net.mcreator.theunknown.TheUnknownMod;
 public class TheUnknownModSounds {
 	public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, TheUnknownMod.MODID);
 	public static final RegistryObject<SoundEvent> METROID = REGISTRY.register("metroid", () -> new SoundEvent(new ResourceLocation("the_unknown", "metroid")));
+	public static final RegistryObject<SoundEvent> ZEBES = REGISTRY.register("zebes", () -> new SoundEvent(new ResourceLocation("the_unknown", "zebes")));
+	public static final RegistryObject<SoundEvent> PIGS = REGISTRY.register("pigs", () -> new SoundEvent(new ResourceLocation("the_unknown", "pigs")));
+	public static final RegistryObject<SoundEvent> OVERSIDE = REGISTRY.register("overside", () -> new SoundEvent(new ResourceLocation("the_unknown", "overside")));
 }
